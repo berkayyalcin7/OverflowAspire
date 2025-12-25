6d94d013b3f8c3b7324c0ca9b84467920cb5fbf7
Berkay <byalcin@ykk.com.tr>
2025-12-25 16:40:42 +0300

    Committing Question service

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -4,4 +4,9 @@ var keycloak = builder.AddKeycloak("keycloak",6001)
     .WithDataVolume("keycloak-data");
 
 
+var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
+    .WithReference(keycloak)
+    .WaitFor(keycloak);
+
+
 builder.Build().Run();
---
+++ Services/QuestionService/Program.cs
@@ -0,0 +1,39 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+// Metrics , Telemetry
+builder.AddServiceDefaults();
+
+// Keycloak Authentication
+builder.Services
+    .AddAuthentication()
+    .AddKeycloakJwtBearer(serviceName: "keycloak", realm: "overflow", options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.Audience = "overflow";
+        // Bunu eklemediğimizde Invalid issuer hatası alıyoruz. 
+        options.Authority = "http://keycloak:6001/realms/overflow";
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapControllers();
+
+app.MapDefaultEndpoints();
+
+app.Run();
